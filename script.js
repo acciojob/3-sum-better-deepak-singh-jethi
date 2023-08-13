fd5eba8ebc0ee1113a190ccddea0f2cb1b4244ab
DEEPAK SINGH JETHI <134581381+deepak-singh-jethi@users.noreply.github.com>
2023-08-13 12:49:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-function threeSum(arr, target) {
-// write your code here
+function threeSum(arr, x) {
+  arr.sort((a,b)=>a-b);
+	let closestSum = 1000000000;
+	for(let i = 0; i<arr.length-2;i++){
+		let start = i+1;
+		let end = arr.length-1;
+		while(start<end){
+			let sum = arr[i]+arr[start]+arr[end];
+			if(Math.abs(1*x-sum)<Math.abs(1*x-closestSum){
+				closestSum = sum;
+			}
+			if(sum>x){
+				end--;
+			}
+			else{
+				start++;
+			}
+			
+		}
+	}
+	return closestSum;
   
 }
 
